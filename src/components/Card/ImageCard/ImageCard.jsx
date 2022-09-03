8e1579fb7c5480cd57977913024d14e1c6691f44
ElenaVolkova4 <tigra_v_kletke@mail.ru>
2022-09-03 12:03:48 +0300

    changed some styles

--- src/components/Card/ImageCard/ImageCard.jsx
+++ src/components/Card/ImageCard/ImageCard.jsx
@@ -1,14 +1,20 @@
 import { useContext } from "react";
 import { imgContext } from "../../../context/imgContext";
 import CardBG from "../../../images/card-bg.jpg";
-import style from "./Felicitation.module.css";
+import style from "./ImageCard.module.css";
 
 const ImageCard = (props) => {
   const { urlImg } = useContext(imgContext);
   // console.log(urlImg);
 
   return (
-    <img src={urlImg || CardBG} alt="Фон открытки" width={840} height={520} />
+    <img
+      src={urlImg || CardBG}
+      alt="Фон открытки"
+      // width={840}
+      // height={520}
+      className={style.image}
+    />
   );
 
   // если через пропсы return <img src={props.img} alt="Фон открытки" width={840} height={520} />;
